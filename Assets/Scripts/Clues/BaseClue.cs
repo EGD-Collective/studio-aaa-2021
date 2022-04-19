68977f1a14ba97e9577043cacd56c870f67c3432
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2022-04-19 15:28:50 -0400

    Merge?

--- Assets/Scripts/Clues/BaseClue.cs
+++ Assets/Scripts/Clues/BaseClue.cs
@@ -9,12 +9,9 @@ namespace Assets.Scripts
         public UnityEvent OnClueActivated;
         public bool Activated;
         public bool SetActiveOnStageStart;
-<<<<<<< HEAD:Assets/Scripts/Clues/BaseClue.cs
-=======
         [SerializeField]
         private NotebookEntrySO clueNote;
 
->>>>>>> feature/EX-103/Note-book:Assets/Scripts/BaseClue.cs
         public virtual void Activate()
         {
             if (!Activated)
